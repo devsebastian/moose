a678befe29ae603508fb0c78554db88ab6280eb8
Dev Sebastian <devsebastian@gmail.com>
2020-02-25 23:16:57 +0530

    better editor with tab indicator

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1,43 +1,27 @@
-import CodeMirror from 'codemirror'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
 import './default.css'
 import React from 'react'
-import 'codemirror/mode/javascript/javascript'
 import './editor.css'
 
 
 class Editor extends React.Component {
-
-    constructor() {
-        super()
-    }
-
-    componentDidMount() {
-        this.cm = new CodeMirror.fromTextArea(document.getElementById(this.props.id), {
-            lineNumbers: true,
-            value: this.props.value,
-            mode: {
-                name: "javascript",
-                json: true
-            },
-            theme: "default",
-        })
-    }
-
     render() {
-        if (this.cm != undefined && this.props.value != undefined) {
-            this.cm.setValue(this.props.value)
-        }
-        return (
-            <textarea id={this.props.id} value={this.props.value} className="view-area" />
-        )
+        return <CodeMirror
+            value={this.props.value}
+            options={{
+                smartIndent: true,
+                indentWithTabs: true,
+                indentUnit: 4,
+                mode: {name: "javascript", json: true},
+                theme: "default",
+                lineNumbers: true
+            }}
+            onChange={(editor, data, value) => {
+            }}
+        />
     }
-
-    componentWillUnmount() {
-        if (this.cm != undefined)
-            this.cm.toTextArea();
-    }
-
 }
 
 export default Editor;
